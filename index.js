6a1364a67c118f4097a7d6287b4a47aa0cf91464
Dominic Tarr <dominic.tarr@gmail.com>
2013-08-07 16:28:53 +0100

    use options object, instead of list of args

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
-
 var Scuttlebutt = require('scuttlebutt')
 var sort = require('scuttlebutt/util').sort
+var inherits = require('util').inherits
 
-function LocalStorageScuttlebutt (id, prefix) {
-  this.prefix = prefix || '_lss'
+module.exports = LocalStorageScuttlebutt
 
-  this.rx = new RegExp('^' + prefix)
+inherits(LocalStorageScuttlebutt, Scuttlebutt)
+
+function LocalStorageScuttlebutt (opts) {
+  opts = opts || {}
+  this.store = opts.store || localStorage
+  this.opts = opts
+  this.prefix = opts.prefix || '_lss'
+
+  var id = (
+          opts.id
+        || localStorage['_id:' + this.prefix]
+        || 'ID_' + Date.now() + Math.random()
+      )
+
+  this.store['_id:' + this.prefix] = opts.id
+
+  this.rx = new RegExp('^' + this.prefix)
 
   Scuttlebutt.call(this, id)
+
   if('undefined' !== typeof window) {
     var self = this
     window.addEventListener('storage', function (ev) {
       if(!ev) ev = window.event //Internet Explorer WTF
+      if(!self.rx.test(ev.key)) return
+      var key = ev.key.substring(self.prefix.length)
       try {
-        var key = ev.key
         var value = JSON.parse(ev.newValue)
         self.emit('change', key, value[0])
         self.emit('change:'+key, value[0])
       } catch (err) {
         console.error(err)
       }
-    }
+    })
   }
 }
 
 var LSS = LocalStorageScuttlebutt.prototype
 
+LSS.keys = function () {
+  var l = this.prefix.length
+  var length = this.store.length
+  var keys = []
+  for(var i = 0; i < length; i++) {
+    var k = this.store.key(i)
+    if(this.rx.test(k))
+      keys.push(k.substring(l))
+  }
+  return keys
+}
+
 LSS.history = function () {
   var hist = []
   var l = this.prefix.length
-  var length = localStorage.length
+  var length = this.store.length
   for(var i = 0; i < length; i++) {
-    var k = localStorage.key(i)
+    var k = this.store.key(i)
     if(this.rx.test(k)) {
       try {
-        var val = JSON.parse(localStorage[k])
+        var val = JSON.parse(this.store[k])
 
         //[value, source, ts] <-- stored like this
         val[0] = [k.substring(l), val[0]]
@@ -52,6 +81,28 @@ LSS.applyUpdate = function (update) {
   var change = update[0]
   var key    = change[0]
   update[0] = change[1]
-  localStorage[this.prefix + key] = JSON.stringify(update)
+  this.store[this.prefix + key] = JSON.stringify(update)
+
+  this.emit('change', key, change[1])
+  this.emit('change:'+key, change[1])
+
   return true
 }
+
+LSS.get = function (key) {
+  var val = this.store[this.prefix + key]
+  if(!val) return
+  try {
+    return JSON.parse(val)[0]
+  } catch (err) {
+    console.error(err)
+    return
+  }
+}
+
+LSS.set = function (key, value) {
+  this.localUpdate([key, value])
+  return this
+}
+
+
